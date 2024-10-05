8bac13d3d0a827bba2bab15bf99c811e448fdaa7
Alan Lima <alan.limagalvez@yahoo.com>
2024-10-04 20:14:06 -0600

    First commit

---
+++ remove_snaps.sh
@@ -0,0 +1,10 @@
+sudo rm -rf /var/cache/snapd/
+
+sudo apt autoremove --purge snapd
+
+rm -rf ~/snap
+sudo rm -rf /snap
+sudo rm -rf /var/snap
+sudo rm -rf /var/lib/snapd
+
+sudo apt-mark hold snapd
